503d4a1bc51aead3260d398cce1f1ff1a4519221
kellyblaze <kellyblazeent@gmail.com>
2026-05-02 23:49:11 -0400

    Fix Supabase connection

--- App.jsx
+++ App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { supabase } from './supabase'
 
 const COLORS = {
--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabaseUrl = 'https://egaxolujduyhcomkbuum.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnYXhvbHVqZHV5aGNvbWtidXVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3NjAzOTUsImV4cCI6MjA5MzMzNjM5NX0.nNWcDR2EVLODNnXnKoRvyJ9YDl63YRMlypimNyG8JXA'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
